9870d063a02d56b5bb326d42b87d63959b7d8391
Kevin Lewis <klewis@nowsecure.com>
2023-04-17 11:48:03 -0400

    Initial Commit

---
+++ iOSVoteApp/iOSVoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSVoteAppApp.swift
+//  iOSVoteApp
+//
+//  Created by Kevin Lewis on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSVoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
